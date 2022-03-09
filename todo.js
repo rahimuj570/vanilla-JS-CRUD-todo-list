7193726e65b305170fd380e7abe50b8cd53dbe9e
rahimuj570 <rahimuj570@gmail.com>
2022-03-10 00:46:19 +0600

    data of local storage is show on UI

--- todo.js
+++ todo.js
@@ -16,8 +16,34 @@ const addList = () => {
   } else {
     alert("Please Add A Task");
   }
-
+  showUi();
   getElem("inputField").value = "";
+};
+
+// ====== Show UI List =====
+const showUi = () => {
+  const tbody = getElem("tbody");
+  const getAllDataArray = getLocal();
+
+  getAllDataArray.map((data, index) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${++index}</td>
+              <td>${data}</td>
+              <td id="action">
+                <span id="edit" onclick="editHandler()"
+                  ><i class="fa-solid fa-pen-to-square"></i
+                ></span>
+                <span id="delete" onclick="deleteHandler()"
+                  ><i class="fa-solid fa-trash-can"></i
+                ></span>
+              </td>`;
+    tbody.appendChild(tr);
+  });
+};
+
+// ====== Todo Edit Handler =====
+const editHandler = () => {
+  alert("d");
 };
 
 // ====== Set Local Storage =====
@@ -28,3 +54,4 @@ const getLocal = () => JSON.parse(localStorage.getItem("todo"));
 
 // ====== Global Array ======
 let arr = getLocal();
+showUi();
